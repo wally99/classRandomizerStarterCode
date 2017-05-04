1b171c7efaf72afd19d09325a2921d5194bd53d6
Wilder <wilderbravo2000@gmail.com>
2017-05-04 02:41:13 +0000

    Class Randomizer

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+var students=["Julio", "Jeanette", "Ashley", "Brittney"];
+var teachers=["Justin", "Zach", "Aaron", "Julia", "Alyxe"];
 
-
-
+$("studentButton").click(function()) {
+    var randomstudents = Math.floor(Math.random()*students.length);
+   $("#studentDisplay").html(students[randomstudents]);
+});
+$("teacherButton").click(function()) {
+    var randomteachers = Math.floor(Math.random()*teachers.length);
+   $("#teacherDisplay").html(teachers[randomteachers]);
+});
 });
